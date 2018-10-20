9306195f546bd2be5ab5ee0eeafe0b82591499db
lori <loridean108@gmail.com>
2018-10-20 17:40:37 -0400

    Added drafts, publish button, delete post. Secured site and created a comment model.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.urls import re_path, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^accounts/login/$', views.login, name='login'),
+    re_path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
